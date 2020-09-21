54921eaa857fa8dc41979636d1b51275a8476a26
Rachana R <rachanarrachu28@gmail.com>
2020-09-21 22:22:08 +0530

    Added : Wage for a month calculated

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,28 +1,32 @@
-public class EmployeeWageComputation
+public class EmpWageComputationUC5
 {
-        static final int IS_FULL_TIME=1,IS_PART_TIME=2,WAGE_PER_HR=20;
+        static final int IS_FULL_TIME=1,IS_PART_TIME=2,WAGE_PER_HR=20,WORKING_DAYS=20;
         public static void main(String[] args)
         {
-                int random=(int)Math.floor(Math.random()*10)%3;
-                int workingHrs=0,salary;
 
-                switch(random)
+                int workingHrs=0,salary,totalSalary=0;
+                for(int day=1;day<=WORKING_DAYS;day++)
                 {
-                        case IS_PART_TIME:
-                                                        System.out.println("Employee Is a part time employee");
-                                                        workingHrs=4;
-                                                        break;
-                        case IS_FULL_TIME:
-                                                        System.out.println("Employee Is a full time employee");
-                                                        workingHrs=8;
-                                                        break;
-                        default:
-                                                        System.out.println("Employee Is Absent ");
+                        int random=(int)Math.floor(Math.random()*10)%3;
+
+                        switch(random)
+                        {
+                                case IS_PART_TIME:
+                                                                workingHrs=4;
+                                                                break;
+                                case IS_FULL_TIME:
+                                                                workingHrs=8;
+                                                                break;
+                                default:
+                                                                workingHrs=0;
+                        }
+
+                        salary=WAGE_PER_HR*workingHrs;
+                        totalSalary+=salary;
                 }
 
-                salary=WAGE_PER_HR*workingHrs;
 
-                System.out.println("Employee Earns "+salary);
+                System.out.println("Employee Earns "+totalSalary+" This Month");
 
         }
 }
