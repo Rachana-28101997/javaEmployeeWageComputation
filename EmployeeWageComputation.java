260ae77e75d424e6093832b3e0f67488aee48b92
Rachana R <rachanarrachu28@gmail.com>
2020-09-21 21:45:33 +0530

    Added : Employee attendance is checked

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,7 +1,16 @@
 public class EmployeeWageComputation
 {
+        static final int PRESENT=1;
         public static void main(String[] args)
         {
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION")
+
+                int random=(int)Math.floor(Math.random()*10)%2;
+
+                if(PRESENT==random)
+                        System.out.println("Employee Is Present ");
+                else
+                        System.out.println("Employee Is Absent ");
+
         }
 }
