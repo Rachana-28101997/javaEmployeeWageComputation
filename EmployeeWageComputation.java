c94a0b3e7fe51f8bcf9242e5fc890b81683a1b87
Rachana R <rachanarrachu28@gmail.com>
2020-09-21 22:02:52 +0530

    Added : Part time employee added and salary is calculated

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,15 +1,20 @@
 public class EmployeeWageComputation
 {
-        static final int IS_PRESENT=1,WAGE_PER_HR=20;
+        static final int IS_FULL_TIME=1,IS_PART_TIME=2,WAGE_PER_HR=20;
         public static void main(String[] args)
         {
-                int random=(int)Math.floor(Math.random()*10)%2;
+                int random=(int)Math.floor(Math.random()*10)%3;
                 int workingHrs=0,salary;
 
-                if(IS_PRESENT==random)
+                if(random==IS_FULL_TIME)
                 {
-                        System.out.println("Employee Is Present ");
+                        System.out.println("Employee Is a full time employee");
                         workingHrs=8;
+                }
+                else if(random==IS_PART_TIME)
+                {
+                        System.out.println("Employee Is a part time employee");
+                        workingHrs=4;
                 }
                 else
                         System.out.println("Employee Is Absent ");
