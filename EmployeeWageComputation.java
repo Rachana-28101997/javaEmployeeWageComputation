a11c0935f8976ef63599b064734eeebba0d0f1b3
Rachana R <rachanarrachu28@gmail.com>
2020-09-23 17:45:50 +0530

    Added : Multiple company wages is calculated

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,38 +2,39 @@ public class EmployeeWageComputation
 {
         static final int IS_FULL_TIME=1,IS_PART_TIME=2;
 
-        public static void computeEmpWage(String company, int wagePerHr, int numOfWorkingDays, int maxHoursPerMonth)
+        public  void computeEmpWage(String company, int wagePerHr, int numOfWorkingDays, int maxHoursPerMonth)
         {
                  int workingHrs=0,salary,totalSalary=0,totalWorkingHrs=0,totalWorkingDays=0;
-                while (totalWorkingDays < numOfWorkingDays && totalWorkingHrs <= maxHoursPerMonth)
-                {
-                        totalWorkingDays++;
-                        int random=(int)Math.floor(Math.random()*10)%3;
+               		 while (totalWorkingDays < numOfWorkingDays && totalWorkingHrs <= maxHoursPerMonth)
+                	{
+                        	totalWorkingDays++;
+                        	int random=(int)Math.floor(Math.random()*10)%3;
 
-                        switch(random)
-                        {
-                                case IS_PART_TIME:
-                                                                workingHrs=4;
-                                                                break;
-                                case IS_FULL_TIME:
-                                                                workingHrs=8;
-                                                                break;
-                                default:
-                                                                workingHrs=0;
-                        }
-                        totalWorkingHrs +=workingHrs;
-                        System.out.println("Emp totalWorkingHrs:" +totalWorkingHrs + "Emp totalWorkingDays:" +totalWorkingDays);
-                }
-                totalSalary=totalWorkingHrs*wagePerHr;
+                        	switch(random)
+                        	{
+                                	case IS_PART_TIME:
+                                                workingHrs=4;
+                                                break;
+                                	case IS_FULL_TIME:
+                                                workingHrs=8;
+                                                break;
+                                	default:
+                                       		workingHrs=0;
+                        	}
+                        	totalWorkingHrs +=workingHrs;
+                        	System.out.println("Emp totalWorkingHrs:" +totalWorkingHrs + "Emp totalWorkingDays:" +totalWorkingDays);
+                	}
+             		   totalSalary=totalWorkingHrs*wagePerHr;
 
-                System.out.println("Employee Wage For Company: " +company+" is: "+totalSalary);
+               		    System.out.println("Employee Wage For Company: " +company+" is: "+totalSalary);
         }
 
         public static void main(String[] args)
         {
+		EmployeeWageComputation emp=new EmployeeWageComputation();
 
-                computeEmpWage("HP",20,20,50);
-                computeEmpWage("BridgeLabs",30,20,50);
+                emp.computeEmpWage("HP",20,20,50);
+                emp.computeEmpWage("BridgeLabs",30,20,50);
 
         }
 }
